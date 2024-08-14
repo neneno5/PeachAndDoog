0c8d33f6a6f9754771dd3a92000de8b8214cc3ec
nene <neneno_5@yahoo.co.jp>
2024-08-14 11:10:30 +0900

    add page1

--- app/views.py
+++ app/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 class IndexView(View):
     def get(self, request, *args, **kwargs):
         return render(request, "app/index.html")
+    
+def page1(request):
+    return render(request, "app/page1.html")
