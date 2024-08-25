ed3318d60414f4671b75849483ce89b26f1d4f02
nene <neneno_5@yahoo.co.jp>
2024-08-25 16:05:50 +0900

    add model

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Save_data
 
-# Register your models here.
+admin.site.register(Save_data)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+#セーブデータ
+class Save_data(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='save_user')
+    save_page = models.IntegerField(default=0)
