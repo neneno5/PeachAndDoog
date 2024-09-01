dfb0d1f4f5af8af5b5ca539688f99fa1af48bdaa
nene <neneno_5@yahoo.co.jp>
2024-09-01 22:48:47 +0900

    change path index=>""

--- app/urls.py
+++ app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
-    path("index", views.index, name="index"),
+    path("", views.index, name="index"),
     path("page1", views.page1, name="page1"),
     path("page2", views.page2, name="page2"),
 ]
